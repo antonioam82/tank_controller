438faf88b2beb1b137c05c6e75bba81d379e6551
Antonio <antonioalfonsomartinez255@gmail.com>
2026-03-30 15:11:58 +0200

    Nuevo cambio de velocidad de translacion

--- tanque_pygame.py
+++ tanque_pygame.py
@@ -13,7 +13,7 @@ grid_size = 110
 grid_spacing = 1
 
 # ================= VELOCIDADES (unidades por segundo) =================
-TANK_SPEED        = 6.0    # velocidad de traslación del tanque -> 3.0
+TANK_SPEED        = 5.0    # velocidad de traslación del tanque -> 3.0
 STOP_DECEL        = 0.5    # desaceleración al frenar (unidades/s²) #6.0
 CAM_ROT_SPEED     = 30.0   # grados/s para rotar escena (T/R)
 CAM_TILT_SPEED    = 15.0   # grados/s para inclinar cámara (F/G)
